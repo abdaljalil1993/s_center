4e04c632062bcdf1602053ce5436326d123ec632
abdaljalil debow (fadi debow) <106731845+abdaljalil1993@users.noreply.github.com>
2026-09-21 14:29:04 +0300

    Delete dist directory

--- dist/modules/purchase/schemas.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.purchaseCourseParamSchema = void 0;
-const requestSchemas_1 = require("../../utils/requestSchemas");
-exports.purchaseCourseParamSchema = requestSchemas_1.idParamSchema;
